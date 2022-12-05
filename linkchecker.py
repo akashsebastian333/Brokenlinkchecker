014a2dcd13bafb5b73a6573b1636e25a0832955a
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2022-12-05 21:17:03 +0530

    Update linkchecker.py

--- linkchecker.py
+++ linkchecker.py
@@ -1,70 +1,51 @@
 from bs4 import BeautifulSoup as bs
-import requests,sys,re
+import requests,sys,re,ssl,argparse
 from urllib.parse import urlparse, urljoin
 
-def start():
-    script_links(url)
-    href(url)
-    status(all_urls)
-
-def status(al):
-    print("\n\nprinting status")
-    for a in al:
-        try:
-            code = session.get(a)
-            print(a , code.status_code)
-        except requests.exceptions.ConnectionError:
-            print(a, "\033[1;31;40m check this \033[0m")
-
-def checkurl(url):
-    checkurl = urlparse(url)
-    return bool(checkurl.netloc) and bool(checkurl.scheme)
-
-def href(url):
-    tags = ['a','base','link','base','area']
-    for a_tag in soup.findAll(tags):
-        href = a_tag.attrs.get('href')
-        if href == "" or href is None:
-            continue
-        href = urljoin(url, href)
-        if samedomain not in href:
-            if href not in all_urls:
-                all_urls.append(href)
-    return urls
-
-
-def script_links(url):
-    tags = ['script','img','style','audio','embed','iframe','source','track','video']
-    for script_tag in soup.find_all(tags):
-        src = script_tag.attrs.get('src')
+def process_tags(tags):
+    for tag in soup.find_all(tags):
+        src = tag.attrs.get('src') or tag.attrs.get('href')
         if src == "" or src is None:
-            continue
-        if checkurl(src) == False:
             continue
         src = urljoin(url, src)
         if samedomain not in src:
             if src not in all_urls:
                 all_urls.append(src)
-    return urls
-
+                try:
+                    status = session.get(src).status_code
+                    if status == 200:
+                        print(f'\033[1;32;40m{src} {status}\033[0m')
+                    else:
+                        print(f'\033[1;31;40m{src} {status}\033[0m')
+                except requests.exceptions.SSLError:
+                    print(f'\033[1;31;40m{src} SSL error\033[0m')
+                except requests.exceptions.ConnectionError:
+                    print(f'\033[1;31;40m{src} Connection error\033[0m')
+    return all_urls
 
 if __name__ == "__main__":
-    try:
-        while(True):
-            url = input("Enter the url to scan: ")
-            #url = sys.argv[1]
+    parser = argparse.ArgumentParser(description='Scan a website for URLs')
+    parser.add_argument('-d', '--domain', help='the domain name to scan')
+    args = parser.parse_args()
 
-            if checkurl(url)==True:
-                all_urls = []
-                urls = []
-                session = requests.Session()
-                session.headers["User-Agent"] = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    url = args.domain
+    if url:
+        parsed_url = urlparse(url)
+        if parsed_url.netloc and parsed_url.scheme:
+            all_urls = []
+            session = requests.Session()
+            session.headers["User-Agent"] = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+            try:
                 html = session.get(url).content
                 soup = bs(html, "html.parser")
-                samedomain = urlparse(url).netloc
-                start()
-                break
-            else:
-                print("Not valid url please check the protocol")
-    except KeyboardInterrupt:
-            pass
+                samedomain = parsed_url.netloc
+                process_tags(['a', 'base', 'link', 'base', 'area'])
+                process_tags(['script', 'img', 'style', 'audio', 'embed', 'iframe', 'source', 'track', 'video'])
+            except ssl.CertificateError:
+                print(f'\033[1;31;40m{url} SSL certificate error\033[0m')
+            except requests.exceptions.ConnectionError:
+                print(f'\033[1;31;40m{url} Connection error\033[0m')
+        else:
+            print("Not valid url please check the protocol")
+    else:
+        parser.print_usage()
